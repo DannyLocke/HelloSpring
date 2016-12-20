a4ccd228e99c9716741f8a79068ece5b2ccb9738
DannyLocke <dlocke@mac.com>
2016-12-20 12:51:26 -0600

    Initial commit

---
+++ src/main/java/com/theironyard/HelloSpringJsonController.java
@@ -0,0 +1,20 @@
+package com.theironyard;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by dlocke on 12/20/16.
+ */
+
+@RestController
+
+public class HelloSpringJsonController {
+
+    @RequestMapping(path = "/person.json", method = RequestMethod.GET)
+    public Person jsonHome (String name, String city, int age) {
+        return new Person(name, city, age);
+    }
+
+}
